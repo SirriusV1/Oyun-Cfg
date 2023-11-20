457f83fcfcb3ea7c40eb48ab3c7400ec309408d4
Sirrius <sirriscelebi@hotmail.com>
2023-11-20 18:42:56 +0300

    Update cs.ps1

--- updated/cs.ps1
+++ updated/cs.ps1
@@ -1,5 +1,5 @@
 # Cs oyunu için işlemler yapılıyor...
-Invoke-WebRequest -Uri "https://drive.google.com/uc?id=1iNfubaww_df_A9IlIS-bavKWVqfHt3dw" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+Invoke-WebRequest -Uri "https://drive.google.com/uc?id=1iNfubaww_df_A9IlIS-bavKWVqfHt3dw" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg\ata.cfg"
 
 Write-Host "Islem tamamlandi. Ana menuye donuluyor..." -ForegroundColor Green
 $host.ui.RawUI.WindowTitle = "Indirme Tamamlandi - Cs"

0a457d3b28a2a31f6fb5ed9c7c8e072d253fa82c
Sirrius <sirriscelebi@hotmail.com>
2023-11-21 19:45:58 +0300

    Update Minecraft.ps1

--- updated/Minecraft.ps1
+++ updated/Minecraft.ps1
@@ -1,7 +1,7 @@
 # Minecraft.ps1
 
 # Minecraft icin destek verilmedigini belirt
-Write-Host "Minecraft icin su an destek verilmemektedir!!."-ForegroundColor Yellow
+Write-Host "Minecraft icin su an destek verilmemektedir!!..."-ForegroundColor Yellow
 
 # Ana PowerShell dosyasina geri don
 Start-Sleep -Seconds 3

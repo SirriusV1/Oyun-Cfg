2c447a599cf5e45cb28db7caad68a3a109514807
Sirrius <sirriscelebi@hotmail.com>
2023-11-22 01:43:37 +0300

    Update settings.ps1

--- updated/mc/settings.ps1
+++ updated/mc/settings.ps1
@@ -1,4 +1,7 @@
-﻿# Zip dosyasının indirileceği URL
+﻿$host.ui.RawUI.WindowTitle = "Minecraft Ayarlarını Güncelle"
+Clear-Host
+
+# Zip dosyasının indirileceği URL
 $zipUrl = "https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/mc.zip"
 
 # Hedef klasör
@@ -10,26 +13,30 @@ Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
 
 # Hedef klasörde aynı isimde klasör var mı kontrol et
 if (Test-Path $minecraftFolder -PathType Container) {
-    $confirmation = Read-Host "Hedef klasörde aynı isimde klasör bulunmaktadır. Üzerine yazmak istiyor musunuz? (E/H)"
-    if ($confirmation -eq "E") {
-        # Zip içeriğini çıkart
-        Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+    Write-Host "Hedef klasörde aynı isimde klasör bulunmaktadır."
 
-        # Zip dosyasını sil
-        Remove-Item $zipFilePath -Force
-
-        Write-Host "Minecraft ayarları başarıyla güncellendi."
-    } else {
+    $confirmation = Read-Host "Üzerine yazmak istiyor musunuz? (E/H)"
+    if ($confirmation -eq "H") {
         Write-Host "İşlem iptal edildi."
+        Exit
     }
-} else {
-    # Hedef klasörde aynı isimde klasör bulunmuyorsa, normal çıkartma işlemini yap
-    Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
-
-    # Zip dosyasını sil
-    Remove-Item $zipFilePath -Force
-
-    Write-Host "Minecraft ayarları başarıyla güncellendi."
+    
+    # Hedef klasörü sil
+    Remove-Item $minecraftFolder -Recurse -Force
 }
+
+# Zip içeriğini çıkart
+Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+
+# Zip dosyasını sil
+Remove-Item $zipFilePath -Force
+
+Write-Host "Minecraft ayarları başarıyla güncellendi." -ForegroundColor Green
+
+# Kullanıcıya birkaç saniye beklemesi için mesaj göster
 Start-Sleep -Seconds 3
+
+# Minecraft ayarlarını başka bir PowerShell betiği ile de güncelle
+Write-Host "Minecraft ayarları yükleniyor..."
 PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
+Write-Host "Minecraft ayarları başarıyla yüklendi." -ForegroundColor Green

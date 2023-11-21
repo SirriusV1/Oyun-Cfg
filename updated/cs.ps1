431da531c12d3b85409be8ccf5c362a70f5560ab
Sirrius <sirriscelebi@hotmail.com>
2023-11-22 01:55:05 +0300

    Update cs.ps1

--- updated/cs.ps1
+++ updated/cs.ps1
@@ -1,8 +1,6 @@
-﻿Clear-Host
-$host.ui.RawUI.WindowTitle = "CS"
+﻿$host.ui.RawUI.WindowTitle = "CS"
 # Cs oyunu için işlemler yapılıyor...
 Invoke-WebRequest -Uri "https://drive.google.com/uc?id=1iNfubaww_df_A9IlIS-bavKWVqfHt3dw" -OutFile "C:\Users\sirri\OneDrive\Masaüstü\deneme\cs\ata.cfg"
 
 Write-Host "Islem tamamlandi. Ana menuye donuluyor..." -ForegroundColor Green
-$host.ui.RawUI.WindowTitle = "Indirme Tamamlandi - Cs"
 Start-Sleep -Seconds 2

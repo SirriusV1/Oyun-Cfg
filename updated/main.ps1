c8c7abd6b771f702f50a7d8dde276aca05d3daba
Sirrius <sirriscelebi@hotmail.com>
2023-11-23 00:33:48 +0300

    Update main.ps1

--- updated/main.ps1
+++ updated/main.ps1
@@ -16,27 +16,22 @@ $secim = Read-Host "Lütfen bir numara girin (1-4)"
 # Secime gore yonlendirme yap
 switch ($secim) {
     1 {
-        # Cs secildiginde yapilacak islemler
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/cs.ps1' | Invoke-Expression }"
         break
     }
     2 {
-        # Rust secildiginde yapilacak islemler
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/rust.ps1' | Invoke-Expression }"
         break
     }
     3 {
-        # Minecraft secildiginde yapilacak islemler
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
         break
     }
     4 {
-        # Pubg secildiginde yapilacak islemler
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/pubg.ps1' | Invoke-Expression }"
         break
     }
     default {
-        # Gecersiz bir secim yapildiginda uyarı ver
         Write-Host "Geçersiz bir seçim yaptınız. Lütfen 1-4 arasinda bir numara girin." -ForegroundColor Red
         Start-Sleep -Seconds 1
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"

9183e94ad163f23dc1461a809d8af2c73ba11f45
Sirrius <sirriscelebi@hotmail.com>
2023-12-25 15:03:40 +0300

    Update rust.ps1

--- updated/rust.ps1
+++ updated/rust.ps1
@@ -1,4 +1,3 @@
-
 $host.ui.RawUI.WindowTitle = "Rust"
 Clear-Host
 
@@ -14,35 +13,66 @@ Write-Host "8. Geri Dön" -ForegroundColor Cyan
 
 $player_choice = Read-Host "Lütfen bir oyuncu seçin (1-8)"
 
+# Define the base URL for CFG files
+$baseCfgUrl = "https://drive.google.com/u/0/uc?id="
+
+# Define the file name
+$fileName = "ata.cfg"
+
+# Define multiple target directories
+$targetDirectories = @(
+    "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\",
+    "C:\SteamLibrary\steamapps\common\Rust\cfg\",
+    "E:\SteamLibrary\steamapps\common\Rust\cfg\",
+    # Ek hedef dizinleri burada ekleyebilirsiniz
+)
+
 switch ($player_choice) {
     "1" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1VWUYc-L5yq6T7ipNNieAA0G9kpOf02gE&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1VWUYc-L5yq6T7ipNNieAA0G9kpOf02gE"
     }
     "2" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1bWyD709q0tb8R2yUm0_b6_sggO8TQftH&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1bWyD709q0tb8R2yUm0_b6_sggO8TQftH"
     }
     "3" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1u1Ol1tm9SFPUzOrNjMK0jnnpykIZhklv&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1u1Ol1tm9SFPUzOrNjMK0jnnpykIZhklv"
     }
     "4" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1LWxIO5JvwRIqt9-ywIS9kGh6UgTimqhZ&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1LWxIO5JvwRIqt9-ywIS9kGh6UgTimqhZ"
     }
     "5" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1b3rAzOlDQVa8LoIn8Een8-FpcxYQrKLA&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1b3rAzOlDQVa8LoIn8Een8-FpcxYQrKLA"
     }
     "6" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1zmcK4soNCz4T2ZEvxXoPESSUN2G0S_xe&export=download" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1zmcK4soNCz4T2ZEvxXoPESSUN2G0S_xe"
     }
     "7" {
-        Invoke-WebRequest -Uri "https://drive.google.com/u/0/uc?id=1L3FmXtfaD3OCOF3_4Ra7IdaO8-n_tO_J&export=downloads" -OutFile "C:\Program Files (x86)\Steam\steamapps\common\Rust\cfg\ata.cfg"
+        $fileId = "1L3FmXtfaD3OCOF3_4Ra7IdaO8-n_tO_J"
     }
     "8" {
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"
+        exit
     }
     default {
         Write-Host "Geçersiz oyuncu seçeneği! Lütfen tekrar deneyin." -ForegroundColor Red
         Start-Sleep -Seconds 2
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/rust.ps1' | Invoke-Expression }"
+        exit
+    }
+}
+
+# İndirilecek dosyanın tam URL'sini oluştur
+$fileUrl = "$baseCfgUrl$fileId&export=download"
+
+# Her bir hedef dizini için dosyayı indir
+foreach ($targetDirectory in $targetDirectories) {
+    $fullPath = Join-Path $targetDirectory $fileName
+
+    # Eğer dosya zaten varsa bu yolu kullan
+    if (Test-Path $fullPath) {
+        # Dosyayı indir
+        Invoke-WebRequest -Uri $fileUrl -OutFile $fullPath
+        break
     }
 }
 

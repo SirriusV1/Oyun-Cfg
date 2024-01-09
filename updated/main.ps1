e897ed2c6195bf3fb169b3f0aac897ae8cd18df1
Sirrius <sirriscelebi@hotmail.com>
2024-01-09 23:24:14 +0300

    Update main.ps1

--- updated/main.ps1
+++ updated/main.ps1
@@ -4,7 +4,7 @@ Clear-Host
 
 Write-Host "Hangi oyunun CFG dosyasini indirmek istiyorsunuz? "
 Write-Host "1. Cs        " -NoNewline
-Write-Host "1.0.4 [06.12.2023]" -ForegroundColor Green
+Write-Host "1.0.4 [09.01.2024]" -ForegroundColor Green
 Write-Host "2. Rust      " -NoNewline
 Write-Host "1.0.5 [25.12.2023]" -ForegroundColor Green
 Write-Host "3. Minecraft " -NoNewline

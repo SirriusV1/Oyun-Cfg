47179f139fe2f9bdd06734feed8262cb31a4c8b9
Sirrius <sirriscelebi@hotmail.com>
2024-03-13 23:21:45 +0300

    Update forge.ps1

--- updated/mc/forge.ps1
+++ updated/mc/forge.ps1
@@ -16,13 +16,22 @@ $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($zipUrl, "$minecraftFolder\$fileName")
 
 # Zip dosyasını çıkart
-Expand-Archive -Path "$minecraftFolder\$fileName" -DestinationPath $minecraftFolder -Force
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+$zipFile = [System.IO.Compression.ZipFile]::OpenRead("$minecraftFolder\$fileName")
+foreach ($entry in $zipFile.Entries) {
+    $entryPath = Join-Path $minecraftFolder $entry.FullName
+    if (-not (Test-Path $entryPath)) {
+        $null = [System.IO.Directory]::CreateDirectory([System.IO.Path]::GetDirectoryName($entryPath))
+        $fileStream = [System.IO.File]::Create($entryPath)
+        $entry.Open().CopyTo($fileStream)
+        $fileStream.Close()
+    }
+}
 
 # "versions" klasörünü hedef klasöre taşı
-Move-Item -Path "$minecraftFolder\versions\*" -Destination $minecraftFolder -Force
-
-# "versions" klasörünü silecek
-Remove-Item -Path "$minecraftFolder\versions" -Force
+if (Test-Path "$minecraftFolder\versions" -PathType Container) {
+    Move-Item -Path "$minecraftFolder\versions\*" -Destination $minecraftFolder -Force
+}
 
 # Zip dosyasını sil
 Remove-Item "$minecraftFolder\$fileName" -Force
@@ -31,3 +40,5 @@ Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
 
 Start-Sleep -Seconds 1
 PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
+
+Read-Host "Devam etmek için bir tuşa basın..."

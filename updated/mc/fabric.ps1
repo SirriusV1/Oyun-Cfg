29fe19a3ad225c4ade48a5b77488083dfe6c3006
Sirrius <sirriscelebi@hotmail.com>
2024-03-13 21:59:03 +0300

    Minecraft test

---
+++ updated/mc/fabric.ps1
@@ -0,0 +1,39 @@
+﻿
+$host.ui.RawUI.WindowTitle = "Fabric Yükleniyor..."
+Clear-Host
+
+# Zip dosyasının indirileceği URL
+$zipUrl = "https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/[ATA] Fabric (1.20.1).rar"
+
+# Hedef klasör
+$minecraftFolder = "$env:USERPROFILE\AppData\Roaming\.minecraft"
+
+# Zip dosyasını indir
+$zipFilePath = Join-Path $minecraftFolder "[ATA] Fabric (1.20.1).rar"
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
+
+# Hedef klasörde aynı isimde klasör var mı kontrol et
+if (Test-Path $minecraftFolder -PathType Container) {
+    $confirmation = Read-Host "Hedef klasörde aynı isimde klasör bulunmaktadır. Üzerine yazmak istiyor musunuz? (E/H)"
+    if ($confirmation -eq "E") {
+        # Zip içeriğini çıkart
+        Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+
+        # Zip dosyasını sil
+        Remove-Item $zipFilePath -Force
+
+        Write-Host "Fabric başarıyla yüklendi." -ForegroundColor Cyan
+    } else {
+        Write-Host "İşlem iptal edildi." -ForegroundColor Red
+    }
+} else {
+    # Hedef klasörde aynı isimde klasör bulunmuyorsa, normal çıkartma işlemini yap
+    Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+
+    # Zip dosyasını sil
+    Remove-Item $zipFilePath -Force
+
+    Write-Host "Fabric başarıyla yüklendi." -ForegroundColor Cyan
+}
+Start-Sleep -Seconds 1
+PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
--- updated/minecraft.ps1
+++ updated/minecraft.ps1
@@ -1,29 +1,35 @@
-
+﻿clear-host
 $host.ui.RawUI.WindowTitle = "Minecraft Menü"
-Clear-Host
+Write-Host "        ╔═════════════════╗"  -ForegroundColor DarkYellow
+Write-Host "        ║  Minecraft Menü ║"  -ForegroundColor DarkYellow
+Write-Host "        ╚═════════════════╝"  -ForegroundColor DarkYellow
+Write-Host ""
+Write-Host "1. Fabric (" -NoNewline
+Write-Host "Önerilen" -ForegroundColor Green -NoNewline
+Write-Host ") "
+Write-Host "2. Forge "
+Write-Host "3. Güncel Serverler "
+Write-Host "4. TLauncher Ayarları "
+Write-Host "5. Geri Dön" -ForegroundColor Cyan
+$secim = Read-Host "Lütfen bir numara girin (1-4)"
 
-Write-Host "1. Minecraft Ayarlarını Yükle"
-Write-Host "2. Modları Yükle"
-Write-Host "3. Journeymap Ayarlarını Yükle"
-Write-Host "4. Geri Dön" -ForegroundColor Cyan
-$secim = Read-Host "Lütfen yapmak istediğiniz işlemi seçin (1-4)"
 
 # Seçime göre işlem yap
 switch ($secim) {
     1 {
-        PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/settings.ps1' | Invoke-Expression }"
+        PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/fabric.ps1' | Invoke-Expression }"
     }
     2 {
-        PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/mod.ps1' | Invoke-Expression }"
+        PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/forge.ps1' | Invoke-Expression }"
     }
-    3 {
+    10 {
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/map.ps1' | Invoke-Expression }"
     }
-    4 {
+    11 {
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"
     }
     default {
-        Write-Host "Geçersiz bir seçim yaptınız. Lütfen 1-4 arasında bir numara girin." -ForegroundColor Red
+        Write-Host "Geçersiz bir seçim yaptınız. Lütfen 1-5 arasında bir numara girin." -ForegroundColor Red
         Start-Sleep -Seconds 1
         PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
     }

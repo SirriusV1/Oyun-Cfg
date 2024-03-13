d4cb5bb23dd8d8c0bd3e1608f1ae723ebfb75a5b
Sirrius <sirriscelebi@hotmail.com>
2024-03-13 22:51:28 +0300

    mc

--- updated/mc/forge.ps1
+++ updated/mc/forge.ps1
@@ -1,4 +1,5 @@
-﻿$host.ui.RawUI.WindowTitle = "Forge Yükleniyor..."
+﻿
+$host.ui.RawUI.WindowTitle = "Forge Yükleniyor..."
 Clear-Host
 
 # Zip dosyasının indirileceği URL
@@ -7,31 +8,22 @@ $zipUrl = "https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/mc/
 # Hedef klasör
 $minecraftFolder = "$env:USERPROFILE\AppData\Roaming\.minecraft"
 
-# İndirme işlemi
-curl -o "$minecraftFolder\Forge.zip" $zipUrl
+# Zip dosyasını indir
+$zipFilePath = Join-Path $minecraftFolder "Forge.zip"
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
 
-# Hedef klasörde aynı isimde klasör var mı kontrol et
-if (Test-Path $minecraftFolder -PathType Container) {
-    $confirmation = Read-Host "Hedef klasörde aynı isimde klasör bulunmaktadır. Üzerine yazmak istiyor musunuz? (E/H)"
-    if ($confirmation -eq "E") {
-        # Zip içeriğini çıkart
-        Expand-Archive -Path "$minecraftFolder\Forge.zip" -DestinationPath $minecraftFolder -Force
+# Zip dosyasını çıkart
+Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
 
-        # Zip dosyasını sil
-        Remove-Item "$minecraftFolder\Forge.zip" -Force
-
-        Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
-    } else {
-        Write-Host "İşlem iptal edildi." -ForegroundColor Red
-    }
-} else {
-    # Hedef klasörde aynı isimde klasör bulunmuyorsa, normal çıkartma işlemini yap
-    Expand-Archive -Path "$minecraftFolder\Forge.zip" -DestinationPath $minecraftFolder -Force
-
-    # Zip dosyasını sil
-    Remove-Item "$minecraftFolder\Forge.zip" -Force
-
-    Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
+# Eğer "versions" klasörü varsa, hedef klasöre taşı
+if (Test-Path "$minecraftFolder\versions" -PathType Container) {
+    Move-Item -Path "$minecraftFolder\versions" -Destination $minecraftFolder -Force
 }
+
+# Zip dosyasını sil
+Remove-Item $zipFilePath -Force
+
+Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
+
 Start-Sleep -Seconds 1
 PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"

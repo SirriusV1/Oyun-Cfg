3a8e8f1ba21c0a7790045cf96243ceade979f485
Sirrius <sirriscelebi@hotmail.com>
2024-03-13 23:45:43 +0300

    Update forge.ps1

--- updated/mc/forge.ps1
+++ updated/mc/forge.ps1
@@ -8,32 +8,24 @@ $zipUrl = "https://github.com/SirriusV1/Oyun-Cfg/raw/main/updated/mc/Force.zip"
 # Hedef klasör
 $minecraftFolder = "$env:USERPROFILE\AppData\Roaming\.minecraft"
 
-# Zip dosyasını indir
-$zipFilePath = Join-Path $minecraftFolder "Forge.zip"
-Invoke-WebRequest -Uri $zipUrl -OutFile $zipFilePath
+# Dosya adı
+$fileName = "Forge.zip"
 
-# Hedef klasörde aynı isimde klasör var mı kontrol et
-if (Test-Path $minecraftFolder -PathType Container) {
-    $confirmation = Read-Host "Hedef klasörde aynı isimde klasör bulunmaktadır. Üzerine yazmak istiyor musunuz? (E/H)"
-    if ($confirmation -eq "E") {
-        # Zip içeriğini çıkart
-        Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+# İndirme işlemi
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($zipUrl, "$minecraftFolder\$fileName")
 
-        # Zip dosyasını sil
-        Remove-Item $zipFilePath -Force
+# Zip dosyasını çıkart
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$minecraftFolder\$fileName", $minecraftFolder)
 
-        Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
-    } else {
-        Write-Host "İşlem iptal edildi." -ForegroundColor Red
-    }
-} else {
-    # Hedef klasörde aynı isimde klasör bulunmuyorsa, normal çıkartma işlemini yap
-    Expand-Archive -Path $zipFilePath -DestinationPath $minecraftFolder -Force
+# Zip dosyasını sil
+Remove-Item "$minecraftFolder\$fileName" -Force
 
-    # Zip dosyasını sil
-    Remove-Item $zipFilePath -Force
+Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
 
-    Write-Host "Forge başarıyla yüklendi." -ForegroundColor Cyan
-}
 Start-Sleep -Seconds 1
 PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
+
+
+Read-Host "Devam etmek için bir tuşa basın..."

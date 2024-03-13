5a66bd426b7165594bfa9a41b5da2e34f2a3fb2b
Sirrius <sirriscelebi@hotmail.com>
2024-03-13 22:03:39 +0300

    Update main.ps1

--- updated/main.ps1
+++ updated/main.ps1
@@ -107,55 +107,7 @@ switch ($secim) {
         
     }
     3 {
-        while ($true) {
-            clear-host
-            $host.ui.RawUI.WindowTitle = "Minecraft Menü"
-            Write-Host "        ╔═════════════════╗"  -ForegroundColor DarkYellow
-            Write-Host "        ║  Minecraft Menü ║"  -ForegroundColor DarkYellow
-            Write-Host "        ╚═════════════════╝"  -ForegroundColor DarkYellow
-            Write-Host ""
-            Write-Host "1. Fabric (" -NoNewline
-            Write-Host "Önerilen" -ForegroundColor Green -NoNewline
-            Write-Host ") "
-            Write-Host "2. Forge "
-            Write-Host "3. Güncel Serverler "
-            Write-Host "4. TLauncher Ayarları "
-            Write-Host "5. Geri Dön" -ForegroundColor Cyan
-            $subSecim = Read-Host "Lütfen bir numara girin (1-4)"
-
-            switch ($subSecim) {
-                1 {
-                    $launchoptions = "-malloc=system -USEALLAVAILABLECORES -system.cpu_priority high -gc.incremental_milliseconds 1 -effects.maxgibs -1 -physics.steps 60 -graphics.waves false"
-                    Set-Clipboard -Value $launchoptions
-                    Set-Clipboard -Value $launchoptions
-                    Write-Host "Başlatma Seçenekleri panoya kopyalandı." -ForegroundColor Green
-                    Start-Sleep -Seconds 1
-                    Write-Host "Steam Kütüphane Açılıyor." -ForegroundColor Cyan
-                    Start-Sleep -Seconds 1
-                    Start-Process steam://open/library
-                    Start-Sleep -Seconds 1
-                    break
-                }
-                2 {
-                    PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/rust.ps1' | Invoke-Expression }"
-                }
-                3{
-                    Start-Process "https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1"
-                }
-                4{
-                    PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"
-                }
-                5{
-                    PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"
-                }
-                default {
-                    Write-Host "Geçersiz bir seçim yaptınız. Lütfen 1-4 arasında bir numara girin." -ForegroundColor Red
-                    Start-Sleep -Seconds 1
-                    break
-                }
-            }
-        }
-        
+        PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/minecraft.ps1' | Invoke-Expression }"
     }
     4 {
         while ($true) {

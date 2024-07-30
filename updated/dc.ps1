20b9aa7b082e05f97235e6b8b24a2e8f44f8b096
Sirrius <sirriscelebi@hotmail.com>
2024-07-30 16:48:41 +0300

    Update dc.ps1

--- updated/dc.ps1
+++ updated/dc.ps1
@@ -1,6 +1,8 @@
-﻿$host.ui.RawUI.WindowTitle = "Discord Yönledirmei..."
+﻿
+$host.ui.RawUI.WindowTitle = "Discord Yönledirime..."
 Clear-Host
 
 Start-Process "discord://https://discord.com/channels/148419527825162240/1118943418421362839"
+Write-Host "İşlem tamamlandı. Ana menüye dönülüyor..." -ForegroundColor Green
 Start-Sleep -Seconds 1
 PowerShell.exe -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri 'https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/main.ps1' | Invoke-Expression }"

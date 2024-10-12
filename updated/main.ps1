c8acbf8ab78df1fcea9acecbfd242db5692a805c
Sirrius <sirriscelebi@hotmail.com>
2024-10-12 22:49:00 +0300

    Update main.ps1

--- updated/main.ps1
+++ updated/main.ps1
@@ -319,7 +319,7 @@ switch ($secim) {
             Write-Host "        ╚════════════════╝"  -ForegroundColor Blue
             Write-Host ""
             Write-Host "1. Otomatik Ayar" -ForegroundColor Green
-            Write-Host "2. Discord 13"
+            Write-Host "2. Discord"
             Write-Host "3. Geri Dön" -ForegroundColor Cyan
             $subSecim = Read-Host "Lütfen bir numara girin (1-3)"
 
@@ -345,9 +345,9 @@ switch ($secim) {
                 2 {
                     Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "Start-Process ''discord://''"'
                     Write-Host "Sakin ol hallediyorum.." -ForegroundColor Yellow
-                    Start-Sleep -Seconds 5
+                    Start-Sleep -Seconds 1
                     Start-Process PowerShell.exe -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "& { Invoke-RestMethod -Uri ''https://raw.githubusercontent.com/SirriusV1/Oyun-Cfg/main/updated/discord.ps1'' | Invoke-Expression }" > $null 2> $null' -Verb RunAs
-                    Start-Sleep -seconds 5
+                    Start-Sleep -seconds 1
                     Stop-Process -Name "Discord" -Force
                     Start-Sleep -seconds 1
                     Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "Start-Process ''discord://channels/148419527825162240/148419527825162240''"'

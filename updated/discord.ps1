1c581a9ca479b420ffcdf1752bb597957eb4556e
Sirrius <sirriscelebi@hotmail.com>
2024-10-13 11:24:06 +0300

    Update discord.ps1

--- updated/discord.ps1
+++ updated/discord.ps1
@@ -1,9 +1,9 @@
 $process = Get-Process | Where-Object { $_.Path -like "*Discord.exe" }
 
 if ($process) {
-    Write-Host "Discord çalışıyor. Google DNS ayarlanıyor..."
-    # Discord çalışıyorsa Google DNS'e geçir
-    Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses '8.8.8.8'
+    Write-Host "Discord çalışıyor. Cloudflare DNS ayarlanıyor..."
+    # Discord çalışıyorsa Cloudflare DNS'e geçir
+    Set-DnsClientServerAddress -InterfaceAlias 'Ethernet' -ServerAddresses '1.1.1.1', '1.0.0.1'
 } else {
     Write-Host "Discord çalışmıyor. DNS eski haline döndürülüyor..."
     # Discord çalışmıyorsa eski DNS ayarlarına döndür

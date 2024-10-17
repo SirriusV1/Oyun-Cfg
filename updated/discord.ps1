a1a0ce20d7614fb5994ae19f4a045838fcdfbf38
Sirrius <sirriscelebi@hotmail.com>
2024-10-17 14:38:20 +0300

    Update discord.ps1

--- updated/discord.ps1
+++ updated/discord.ps1
@@ -5,29 +5,17 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     exit
 }
 
-$process = Get-Process | Where-Object { $_.Path -like "*Discord.exe" }
+# Google DNS ayarlanıyor
+Write-Host "1.1.1.1 DNS ayarlanıyor..."
 
-if ($process) {
-    Write-Host "Discord çalışıyor. Google DNS ayarlanıyor..."
-    # Tüm ağ arayüzlerine Google DNS ayarla
-    Get-DnsClient | ForEach-Object {
-        try {
-            Set-DnsClientServerAddress -InterfaceIndex $_.InterfaceIndex -ServerAddresses '8.8.8.8', '8.8.4.4'
-            Write-Host "DNS başarıyla ayarlandı: $($_.InterfaceAlias)"
-        } catch {
-            Write-Host "DNS ayarlanamadı: $($_.InterfaceAlias). Hata: $_"
-        }
-    }
-} else {
-    Write-Host "Discord çalışmıyor. DNS eski haline döndürülüyor..."
-    # Tüm ağ arayüzlerindeki DNS ayarlarını eski haline döndür
-    Get-DnsClient | ForEach-Object {
-        try {
-            Set-DnsClientServerAddress -InterfaceIndex $_.InterfaceIndex -ResetServerAddresses
-            Write-Host "DNS başarıyla sıfırlandı: $($_.InterfaceAlias)"
-        } catch {
-            Write-Host "DNS sıfırlanamadı: $($_.InterfaceAlias). Hata: $_"
-        }
+# Tüm ağ arayüzlerine 1.1.1.1 DNS ayarla
+Get-DnsClient | ForEach-Object {
+    try {
+        Set-DnsClientServerAddress -InterfaceIndex $_.InterfaceIndex -ServerAddresses '1.1.1.1'
+        Write-Host "DNS başarıyla ayarlandı: $($_.InterfaceAlias)"
+    } catch {
+        Write-Host "DNS ayarlanamadı: $($_.InterfaceAlias). Hata: $_"
     }
 }
+
 Start-Sleep -seconds 3

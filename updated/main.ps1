8b031b472bec3b1fc8bbce03abb0af999f50a18b
Sirrius <48409029+SirriusV1@users.noreply.github.com>
2026-01-26 20:32:54 +0300

    Update main.ps1

--- updated/main.ps1
+++ updated/main.ps1
@@ -127,9 +127,9 @@ Write-Host "Hangi oyunun CFG dosyasını indirmek istiyorsunuz? "
 Write-Host "1. Cs        " -NoNewline
 Write-Host "1.0.4 [09.01.2024] " -ForegroundColor DarkRed
 Write-Host "2. Rust      " -NoNewline
-Write-Host "1.0.8 [13.08.2024]" -ForegroundColor Green
-Write-Host "3. Minecraft " -NoNewline
-Write-Host "1.21 Geldi Geldi" -ForegroundColor Yellow
+Write-Host "1.0.8 [13.08.2024]" -ForegroundColor DarkRed
+Write-Host "3. Minecraft 2 " -NoNewline
+Write-Host "1.21.11 " -ForegroundColor Green
 Write-Host "4. Pubg "
 Write-Host "5. Pc Ayar "
 $sec = "(1-5)" 
